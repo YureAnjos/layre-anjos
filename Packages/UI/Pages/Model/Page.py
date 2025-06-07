62ac1a999957c1ebdf0080b4d73644685b534077
Gui97p <guipassaroto07@gmail.com>
2025-06-07 11:23:16 -0400

    feat: Upload Screen GUI integrated

--- Packages/UI/Pages/Model/Page.py
+++ Packages/UI/Pages/Model/Page.py
@@ -4,6 +4,8 @@ class Page(CTkFrame):
     def __init__(self, master):
         super().__init__(master, fg_color='transparent')
         self.loaded = False
+        self.iconify = master.iconify
+        self.deiconify = master.deiconify
     
     def load(self, navigate):
         self.place(relwidth=1, relheight=1)
